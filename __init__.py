59c955d6c9cc0126b3a272acbbafccaa4c41059f
Morgan Johansson <alt.key.project@gmail.com>
2025-02-11 21:48:02 +0100

    update check fix.

--- __init__.py
+++ __init__.py
@@ -53,7 +53,7 @@ _SIGNATURE_SUFFIX = " [DVB]"
 
 MANIFEST = {
     "name": "Dream Video Batches",
-    "version": (1, 0, 7),
+    "version": (1, 0, 8),
     "author": "Dream Project",
     "project": "https://github.com/alt-key-project/comfyui-dream-video-batches",
     "description": "Various utility nodes for working with video batches in ComfyUI",
